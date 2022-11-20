3c72811545e466070cf9f09276eb6139775bfb13
t28hub <t28oogle@gmail.com>
2022-11-20 16:02:14 +0900

    Improve performance of color extraction to resize image

--- src/main.ts
+++ src/main.ts
@@ -2,39 +2,41 @@ import { Color } from './color';
 import { PaletteGenerator } from './palette';
 import { timed } from './utils';
 
+const MAX_IMAGE_SIZE = 192 * 192;
+
 export function fromImage(image: HTMLImageElement): Promise<Color[]> {
   if (!image.complete) {
-    throw new Error(`Image(src=${image.src}) is not loaded`);
+    throw new TypeError(`Image(src=${image.src}) is not loaded`);
   }
 
   const canvas = document.createElement('canvas');
-  canvas.width = image.naturalWidth;
-  canvas.height = image.naturalHeight;
-
   const context = canvas.getContext('2d');
   if (!context) {
     throw new Error('Failed to retrieve 2d context from canvas');
   }
-  context.drawImage(image, 0, 0, canvas.width, canvas.height);
-  return fromCanvas(canvas);
-}
 
-export function fromCanvas(canvas: HTMLCanvasElement): Promise<Color[]> {
-  const context = canvas.getContext('2d');
-  if (!context) {
-    throw new Error('Failed to retrieve 2d context from canvas');
+  const imageWidth = image.naturalWidth;
+  const imageHeight = image.naturalHeight;
+  if (imageWidth === 0 || imageHeight === 0) {
+    throw new TypeError(`The image(src=${image.src}) is empty: width=${imageWidth}, height=${imageHeight}`);
   }
+
+  // Resize the image on main thread since OffscreenCanvas is not supported by some browsers.
+  // [OffscreenCanvas | Can I use...](https://caniuse.com/offscreencanvas)
+  const imageSize = imageWidth * imageHeight;
+  const scale = Math.sqrt(MAX_IMAGE_SIZE / imageSize);
+  const width = Math.floor(imageWidth * scale);
+  const height = Math.floor(imageHeight * scale);
+
+  canvas.width = width;
+  canvas.height = height;
+  context.drawImage(image, 0, 0, width, height);
 
   try {
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height, { colorSpace: 'srgb' });
-    return fromImageData(imageData);
+    const imageData = context.getImageData(0, 0, width, height, { colorSpace: 'srgb' });
+    const generator = new PaletteGenerator();
+    return timed('palette', () => generator.generate(imageData, 10));
   } catch (e) {
-    console.warn('Failed to get ImageData from an image', e);
     throw new Error('Failed to get ImageData from an image');
   }
-}
-
-export function fromImageData(imageData: ImageData): Promise<Color[]> {
-  const generator = new PaletteGenerator();
-  return timed('palette', () => generator.generate(imageData, 10));
 }
