60eb252b7fcef881a0ee3b5aeefd5029e3c04792
t28hub <t28oogle@gmail.com>
2022-12-30 20:02:32 +0900

    Fix incorrect entry point of library

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/photo.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'AutoPalette',
       formats: ['es', 'umd'],
       fileName: (format) => `index.${format}.js`,
