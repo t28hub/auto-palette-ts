9581221a29af48babf9663a0740b40df345d60a5
t28hub <t28oogle@gmail.com>
2023-01-04 23:55:58 +0900

    Add demo project files and remove app project files

--- app/src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement, ReactNode } from 'react';
-
-interface Props {
-  readonly children: ReactNode;
-}
-
-export default function Container({ children }: Props): ReactElement {
-  return <div className="flex flex-col">{children}</div>;
-}
