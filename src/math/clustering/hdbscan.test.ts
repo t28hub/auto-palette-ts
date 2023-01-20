243b9b22298e09062118db561d597a4e67bea747
t28hub <t28oogle@gmail.com>
2023-01-20 10:02:41 +0900

    Implement core distance calculation and MST construction

--- src/math/clustering/hdbscan.test.ts
+++ src/math/clustering/hdbscan.test.ts
@@ -1,14 +1,39 @@
 import { beforeEach, describe, it } from 'vitest';
 
+import { euclidean } from '../distance';
 import { Point2 } from '../types';
 
 import { HDBSCAN } from './hdbscan';
+
+const points: Point2[] = [
+  [0, 0],
+  [0, 1],
+  [0, 7],
+  [0, 8],
+  [1, 0],
+  [1, 1],
+  [1, 2],
+  [1, 7],
+  [1, 8],
+  [2, 1],
+  [2, 2],
+  [4, 3],
+  [4, 4],
+  [4, 5],
+  [5, 3],
+  [5, 4],
+  [8, 4],
+  [8, 4],
+  [8, 5],
+  [8, 7],
+  [8, 8],
+];
 
 describe('HDBSCAN', () => {
   describe('constructor', () => {
     it('should create a HDBSCAN', () => {
       // Act
-      const actual = new HDBSCAN();
+      const actual = new HDBSCAN(16, euclidean());
 
       // Assert
       expect(actual).toBeDefined();
@@ -18,7 +43,15 @@ describe('HDBSCAN', () => {
   describe('fit', () => {
     let hdbscan: HDBSCAN<Point2>;
     beforeEach(() => {
-      hdbscan = new HDBSCAN<Point2>();
+      hdbscan = new HDBSCAN<Point2>(4, euclidean());
+    });
+
+    it('should return clusters', () => {
+      // Act
+      const actual = hdbscan.fit(points);
+
+      // Assert
+      expect(actual).toBeEmpty();
     });
 
     it('should return an empty clusters if the given points is empty', () => {
--- src/math/clustering/hdbscan.ts
+++ src/math/clustering/hdbscan.ts
@@ -1,12 +1,32 @@
-import { Cluster, Clustering, Point } from '../types';
+import { PriorityQueue } from '../../utils';
+import { kdtree } from '../neighbor';
+import { Cluster, Clustering, DistanceFunction, Point } from '../types';
+
+interface Edge {
+  readonly from: number;
+  readonly to: number;
+  readonly weight: number;
+}
 
 /**
  * Hierarchical density-based spatial clustering of applications with noise implementation.
  *
  * @param P The type of point.
- * @see [Wikipedia - DBSCAN](https://hdbscan.readthedocs.io/en/latest/how_hdbscan_works.html)
+ * @see [How HDBSCAN Works](https://hdbscan.readthedocs.io/en/latest/how_hdbscan_works.html)
  */
 export class HDBSCAN<P extends Point> implements Clustering<P> {
+  /**
+   * Create a new HDBSCAN.
+   *
+   * @param minPoints The number of min points to compute core distance.
+   * @param distanceFunction The distance function.
+   */
+  constructor(private readonly minPoints: number, private readonly distanceFunction: DistanceFunction<P>) {
+    if (minPoints < 1) {
+      throw new RangeError(`The number of min points is less than 1: ${minPoints}`);
+    }
+  }
+
   /**
    * {@inheritDoc Clustering.fit}
    */
@@ -14,6 +34,58 @@ export class HDBSCAN<P extends Point> implements Clustering<P> {
     if (points.length === 0) {
       return [];
     }
-    throw new Error('HDBSCAN algorithm is not implemented.');
+
+    const coreDistances = this.createCoreDistances(points);
+    const minimumSpanningTree = this.buildMinimumSpanningTree(points, coreDistances);
+    console.table(minimumSpanningTree);
+    return [];
+  }
+
+  private createCoreDistances(points: P[]): Float64Array {
+    const k = Math.min(points.length, this.minPoints);
+    const neighborSearch = kdtree(points, this.distanceFunction);
+    return points.reduce((distances: Float64Array, point: P, index: number): Float64Array => {
+      const neighbors = neighborSearch.search(point, k);
+      const coreNeighbor = neighbors.pop();
+      if (!coreNeighbor) {
+        throw new Error(`No neighbor point corresponding at ${index} was found`);
+      }
+      distances[index] = coreNeighbor.distance;
+      return distances;
+    }, new Float64Array(points.length));
+  }
+
+  private buildMinimumSpanningTree(points: P[], coreDistance: Float64Array): Edge[] {
+    const edges = new Array<Edge>();
+    const attached = new Set<number>();
+    const candidates = new PriorityQueue((edge: Edge): number => -edge.weight);
+    let currentIndex = points.length - 1;
+    attached.add(currentIndex);
+    while (attached.size < points.length) {
+      points.forEach((point: P, index: number) => {
+        if (currentIndex == index || attached.has(index)) {
+          return;
+        }
+
+        const distance = this.distanceFunction.measure(point, points[currentIndex]);
+        const mutualReachabilityDistance = Math.max(distance, coreDistance[index], coreDistance[currentIndex]);
+        candidates.enqueue({ from: currentIndex, to: index, weight: mutualReachabilityDistance });
+      });
+
+      while (!candidates.isEmpty) {
+        const shortest = candidates.dequeue();
+        if (!shortest) {
+          throw new Error(`No shortest edge at ${currentIndex} was found`);
+        }
+
+        if (!attached.has(shortest.to)) {
+          edges.push(shortest);
+          attached.add(shortest.to);
+          currentIndex = shortest.to;
+          break;
+        }
+      }
+    }
+    return edges;
   }
 }
