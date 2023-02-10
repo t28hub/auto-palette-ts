67cdfb77915b3bb8f9b317112217ad83c870cdce
t28hub <t28oogle@gmail.com>
2023-02-10 23:45:38 +0900

    Change exported interface of AutoPalette

--- src/index.ts
+++ src/index.ts
@@ -12,22 +12,46 @@ export type { Color, ImageSource, Quality, Swatch } from './types';
  * Type representing options for Auto Palette.
  */
 export type Options = {
+  /**
+   * The extraction quality.
+   */
   readonly quality: Quality;
+
+  /**
+   * The max image size.
+   */
   readonly maxImageSize: number;
 };
 
 const DefaultOptions: Options = {
   quality: 'middle',
-  maxImageSize: 128 * 128,
+  maxImageSize: 128 * 128
 };
 
+/**
+ * AutoPalette class for extracting Palette instances.
+ */
 export class AutoPalette {
   private readonly worker: WorkerPromise;
 
+  /**
+   * Create a new AutoPalette.
+   *
+   * @private
+   * @param quality The extraction quality.
+   * @param maxImageSize The max image size.
+   * @param worker The worker instance.
+   */
   private constructor(private readonly quality: Quality, private readonly maxImageSize: number, worker: Worker) {
     this.worker = new WorkerPromise(worker);
   }
 
+  /**
+   * Extract the palette from image source.
+   *
+   * @param source The image source.
+   * @return The promise-based extracted palette.
+   */
   async extract(source: ImageSource): Promise<Palette> {
     const image = createImage(source);
     const resized = await image.resize(this.maxImageSize);
@@ -42,8 +66,8 @@ export class AutoPalette {
         population: point.population,
         coordinate: {
           x: Math.round(point.coordinate.x * scaleX),
-          y: Math.round(point.coordinate.y * scaleY),
-        },
+          y: Math.round(point.coordinate.y * scaleY)
+        }
       };
     });
     return new Palette(swatches);
@@ -57,12 +81,18 @@ export class AutoPalette {
         width: imageData.width,
         height: imageData.height,
         buffer: imageData.data.buffer,
-        quality: this.quality,
-      },
+        quality: this.quality
+      }
     });
     return response.content.points;
   }
 
+  /**
+   * Create a new AutoPalette from options.
+   *
+   * @param options The options for AutoPalette.
+   * @return The new AutoPalette.
+   */
   static create(options: Partial<Options> = {}): AutoPalette {
     const { quality, maxImageSize } = { ...DefaultOptions, ...options };
     return new AutoPalette(quality, maxImageSize, defaultWorker());
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,12 +1,12 @@
 import '@vitest/web-worker';
 import 'jest-extended';
 import 'jest-extended/all';
-import * as crypto from 'node:crypto';
+import { randomUUID } from 'node:crypto';
 
 import './src/test/matchers';
 
 Object.defineProperty(global.self, 'crypto', {
   value: {
-    randomUUID: () => crypto.randomUUID(),
+    randomUUID: () => randomUUID(),
   },
 });
