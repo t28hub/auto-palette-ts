7c725b9791b5dc3dd14e561120c645c6c1205c23
t28hub <t28oogle@gmail.com>
2023-12-11 00:09:30 +0900

    Refactor euclidean function to eliminate duplicate code

--- src/math/distance/euclidean.ts
+++ src/math/distance/euclidean.ts
@@ -11,12 +11,8 @@ import { Distance, DistanceFunction } from './function';
  * @return The Euclidean distance between two points.
  */
 export const euclidean: DistanceFunction = <P extends Point>(point1: P, point2: P): Distance => {
-  const distance = point1.reduce((total: number, value: number, index: number): number => {
-    const delta = point2[index] - value;
-    return total + delta * delta;
-  }, 0.0);
-  assertFiniteNumber(distance, 'Either point1 or point2 contains infinite number');
-  return Math.sqrt(distance) as Distance;
+  const squared = squaredEuclidean(point1, point2);
+  return Math.sqrt(squared) as Distance;
 };
 
 /**
