6a38252890c3ca7bf29f69ae357ded0b90e9ba97
Tatsuya Maki <t28oogle@gmail.com>
2025-03-01 23:03:06 +0900

    chore: bump @napi-rs/canvas from 0.1.52 to 0.1.67

--- test/utils.node.ts
+++ test/utils.node.ts
@@ -1,31 +1,15 @@
-import { readFile } from 'node:fs/promises';
-
-import { Image, createCanvas } from '@napi-rs/canvas';
+import { createCanvas, loadImage } from '@napi-rs/canvas';
 
 /**
  * Load the image from a given file as ImageData object.
  *
- * @param filepath - The path to the image file.
+ * @param path - The path to the image file.
  * @return A Promise that resolves to the loaded image as ImageData object.
- * @see {@link loadImageDataFromUrl}
  */
-export async function loadImageData(filepath: string): Promise<ImageData> {
-  const contents = await readFile(filepath, { flag: 'r' });
-  const image = new Image();
-  image.src = contents;
-  return toImageData(image);
-}
-
-/**
- * Convert an Image object to an ImageData object.
- *
- * @param image - The Image object to be converted.
- * @returns The converted ImageData object.
- */
-function toImageData(image: Image): ImageData {
-  const { width, height } = image;
-  const canvas = createCanvas(width, height);
+export async function loadImageData(path: string): Promise<ImageData> {
+  const image = await loadImage(path);
+  const canvas = createCanvas(image.width, image.height);
   const context = canvas.getContext('2d', { alpha: true, colorSpace: 'srgb' });
-  context.drawImage(image, 0, 0, width, height);
-  return context.getImageData(0, 0, width, height);
+  context.drawImage(image, 0, 0);
+  return context.getImageData(0, 0, image.width, image.height);
 }
